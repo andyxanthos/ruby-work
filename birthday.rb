2a5c63fcfb3a2f6ad118aa2efd9f401dbfc731c2
Andy Xanthos <andykxanthos@gmail.com>
2016-09-29 14:38:07 -0400

    added more functionality to birthday.rb

--- birthday.rb
+++ birthday.rb
@@ -13,11 +13,21 @@ age_secs = current_time - birthday
 age_mins = age_secs / 60
 age_hours = age_mins / 60
 age_days = age_hours / 24
-age = age_days / 365
+age_exact = age_days / 365
+age = age_exact.to_i
 
 puts "Here is your EXACT age in various units of measurement:"
 puts "seconds: #{age_secs}"
 puts "minutes: #{age_mins}"
 puts "hours: #{age_hours}"
 puts "days: #{age_days}"
-puts "years: #{age}"
+puts "years: #{age_exact}"
+puts "So, you're #{age} years old."
+
+if age >= 50
+  puts "With such great age comes great wisdom."
+elsif age < 50 && age >=30
+  puts "You keep doing you, buddy ol' pal"
+else
+  puts "You've got so much more life to look forward to!"
+end
