b7e8e82ea5a87f8eb04eb5165ccfdf5cd668ee33
Andy Xanthos <andykxanthos@gmail.com>
2016-09-30 00:08:50 -0400

    added fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+(0..100).each do |x|
+  if x % 5 == 0 && x % 3 == 0
+    puts x.to_s + " fizzbuzz"
+  elsif x % 3 == 0
+    puts x.to_s + " fizz"
+  elsif x % 5 == 0
+    puts x.to_s + " buzz"
+  else
+    puts x.to_s
+  end
+end
