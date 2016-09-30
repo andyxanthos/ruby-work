51019e5d2635f06db9648957d2d7f2673e36a30b
Andy Xanthos <andykxanthos@gmail.com>
2016-09-30 00:11:07 -0400

    small change to fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 (0..100).each do |x|
   if x % 5 == 0 && x % 3 == 0
-    puts x.to_s + " fizzbuzz"
+    puts x.to_s + " fizzbuzz" unless x == 0
   elsif x % 3 == 0
     puts x.to_s + " fizz"
   elsif x % 5 == 0
